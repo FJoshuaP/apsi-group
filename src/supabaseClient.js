ca55f1005e9e5f306cad0a67840c61067e48f982
kiarsie <kiaramariz712@gmail.com>
2025-10-04 21:46:49 +0800

    Revert "edit"
    This reverts commit e10fe1dfdf53c2b93b2bf27a83fc0efd3bc1c3a7.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,12 @@
 // src/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient('http://72.155.88.223:8000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE', {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    flowType: 'implicit'
+  }
+});
+export default supabase;
